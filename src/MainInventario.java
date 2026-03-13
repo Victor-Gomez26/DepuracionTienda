7d0b8c2af217a0d3c52b6d4945ea798a131216b0
Victor-Gomez26 <gomez.rovic25@cordoba.salesianos.edu>
2026-03-13 17:10:28 +0100

    Arreglado el error de el producto no existente

--- src/MainInventario.java
+++ src/MainInventario.java
@@ -31,7 +31,7 @@ public class MainInventario {
         inventario.listarProductos();
 
         System.out.println("Actualizado el stock de un producto que no existe (RAM) a 7. No debería actualizarm pero tampoco dar error");
-        //inventario.actualizarStock("RAM", 7);
+        inventario.actualizarStock("RAM", 7);
         inventario.listarProductos();
 
         // Manipulamos datos “erróneos” para el valor total
